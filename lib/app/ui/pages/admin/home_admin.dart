4d4d9410dce65443218a1e15051d5710a8f2e80d
NataliRondo <nata10_11@hotmail.com>
2021-11-23 01:05:08 -0500

    actu

--- lib/app/ui/pages/admin/home_admin.dart
+++ lib/app/ui/pages/admin/home_admin.dart
@@ -66,6 +66,7 @@ class _AdminHomeState extends State<AdminHome> {
                     color: const Color(0xFFede7f6),
                     borderRadius: BorderRadius.circular(30.0),
                   ),
+                   
                   child: TableCalendar(
                     selectedDayPredicate: (day) => _selectedDay == day,
                     onDaySelected: (selectedDay, focusedDay) {
